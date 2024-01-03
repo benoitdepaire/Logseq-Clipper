acc48612db7225d6659229a4b923e42e02113e65
Benoît Depaire <benoit@depaire.net>
2024-01-03 23:38:08 +0100

    version 0.0.1

--- background_script.js
+++ background_script.js
@@ -1,1 +1,21 @@
-// Put all the javascript code here, that you want to execute in background.
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      if (command === "activate-extension") {
+          chrome.tabs.sendMessage(tabs[0].id, {command: "toggleExtension"});
+      } else if (command === "activate-alternate-extension") {
+          chrome.tabs.sendMessage(tabs[0].id, {command: "toggleAlternateExtension"});
+      } else if (command === "copy-to-clipboard") {
+          chrome.tabs.sendMessage(tabs[0].id, {command: "copyToClipboard"}, function(response) {
+              copyTextToClipboard(response);
+          });
+      }
+  });
+});
+
+function copyTextToClipboard(text) {
+  navigator.clipboard.writeText(text).then(() => {
+      console.log('Text copied to clipboard');
+  }).catch(err => {
+      console.error('Failed to copy text: ', err);
+  });
+}
--- content_script.js
+++ content_script.js
@@ -1,1 +1,110 @@
-// Put all the javascript code here, that you want to execute after page load.
+function getPageInfo() {
+  const url = window.location.href;
+  const title = document.title;
+  const selectedText = window.getSelection().toString();
+  return { url, title, selectedText };
+}
+
+function updatePopup(newContent, isInitial, useAlternateTemplate) {
+  let popup = document.getElementById('info-popup');
+  if (!popup) {
+      popup = document.createElement('textarea');
+      popup.id = 'info-popup';
+      popup.style.position = 'fixed';
+      popup.style.bottom = '10px';
+      popup.style.right = '10px';
+      popup.style.width = '300px';
+      popup.style.height= '200px';
+      popup.style.backgroundColor = 'white';
+      popup.style.border = '1px solid #ddd';
+      popup.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.5)';
+      popup.style.padding = '15px';
+      popup.style.zIndex = '10000';
+      popup.style.fontSize = '14px';
+      popup.style.overflowY = 'auto';
+      popup.style.whiteSpace = 'pre-wrap';
+
+      // Append the popup to the body
+      document.body.appendChild(popup);
+  }
+
+  if (isInitial) {
+    popup.value = newContent;
+
+    let titleStart = 2
+    if(!useAlternateTemplate){
+      titleStart = 7
+    }
+    const titleEnd = newContent.indexOf('\n', titleStart);
+    //Pre-select the title
+    popup.setSelectionRange(titleStart, titleEnd);
+  } else if (newContent) {
+      popup.value += (popup.value ? '\n' : '') + `${newContent}`;
+  }
+
+  popup.style.display = 'block';
+  popup.focus();
+}
+
+function removePopup() {
+  let popup = document.getElementById('info-popup');
+  if (popup) {
+      //popup.style.display = 'none';
+      // Or, if you prefer to remove it completely:
+      popup.parentNode.removeChild(popup);
+  }
+}
+
+function formatSelectedText(text) {
+  return text.split('\n')
+             .map(line => {
+                 // Remove leading dash if present
+                 const trimmedLine = line.trimStart();
+                 return `  - ${trimmedLine.startsWith('-') ? trimmedLine.substring(1).trimStart() : trimmedLine}`;
+             })
+             .join('\n');
+}
+
+function getCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.command === "toggleExtension" || request.command === "toggleAlternateExtension") {
+      let popup = document.getElementById('info-popup');
+      const useAlternateTemplate = request.command === "toggleAlternateExtension";
+      if (popup) {
+          // Popup exists, format and add selected text
+          const selectedText = window.getSelection().toString();
+          if (selectedText) {
+              const formattedText = formatSelectedText(selectedText);
+              updatePopup(formattedText, false, useAlternateTemplate);
+          }
+      } else {
+          // Popup doesn't exist, create with initial content
+          const pageInfo = getPageInfo();
+          let initialContent;
+          if (useAlternateTemplate) {
+              initialContent = `- ${pageInfo.title}\n  created:: ${getCurrentDate()}\n  ref:: (web-source)[${pageInfo.url}]`;
+          } else {
+              initialContent = `- TODO ${pageInfo.title}\n  created:: ${getCurrentDate()}\n  related-to:: \n  deadline:: \n  ref:: (web-source)[${pageInfo.url}]`;
+          }
+          if (pageInfo.selectedText) {
+              const formattedText = formatSelectedText(pageInfo.selectedText);
+              initialContent += `\n${formattedText}`;
+          }
+          updatePopup(initialContent, true, useAlternateTemplate);
+      }
+  } else if (request.command === "copyToClipboard") {
+    let popup = document.getElementById('info-popup');
+    if (popup) {
+        sendResponse(popup.value);
+        removePopup(); // Remove the popup after copying
+    }
+  }
+});
